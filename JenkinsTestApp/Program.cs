e451a2ccf2f2cf400b8b65fe1f14e0ea23cbf045
Sakana81 <sakana81@yandex.com>
2023-11-14 14:39:48 +0500

    dsafa

--- JenkinsTestApp/Program.cs
+++ JenkinsTestApp/Program.cs
@@ -17,7 +17,7 @@ namespace JenkinsTestApp
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            var a = 1;
+            var a = 2;
         }
     }
 }
