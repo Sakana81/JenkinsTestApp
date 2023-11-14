f1176047778f4a4de2e65e3eec3237f1d4fed23f
Sakana81 <sakana81@yandex.com>
2023-11-14 14:35:07 +0500

    dhsglbdg

--- JenkinsTestApp/Program.cs
+++ JenkinsTestApp/Program.cs
@@ -17,6 +17,7 @@ namespace JenkinsTestApp
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            var a = 1;
         }
     }
 }
